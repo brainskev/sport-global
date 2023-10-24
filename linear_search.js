0b0d0de81aa50cb35dc62892d56061913d309a35
Kelvin <ngotho.kelvin@yahoo.com>
2023-10-24 19:53:08 +0300

    Restore linear.js

--- linear_search.js
+++ linear_search.js
@@ -1,24 +1,20 @@
-
 function linear_search(list, target) {
-
     for (let i = 0; i < list.length; i++) {
-        if (list[i] === target) {
-            return i;
-        }
+        if (list[i] === target)
+            return i
     }
     return null;
 }
 
 function verify(index) {
-    if (index === ! null) {
-        console.log("Target found at index", i)
+    if (index !== null) {
+        console.log("Target found at index", index)
     } else console.log("Target not found in list")
+
 }
 
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-const result = linear_search(numbers, 12);
-
-verify(result);
-
+const result = linear_search(numbers, 17)
+verify(result)
 
