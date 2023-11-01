2162b373fa71267170a88977edbf3664edf20820
Kelvin <ngotho.kelvin@yahoo.com>
2023-11-02 01:01:54 +0300

    Define midpoint

--- recursive_binary_search.rb
+++ recursive_binary_search.rb
@@ -1,1 +1,7 @@
-def recursive4_binary_searh
+def recursive4_binary_search
+    if len(list) == 0
+        return false
+      else midpoint = (len(list))//2
+        if list[midpoint]
+    end
+     
